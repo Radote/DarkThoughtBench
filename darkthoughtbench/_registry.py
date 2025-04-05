c2940465d9e891a4726899b13f876b60a3605a44
Thomas Kiefer <74317464+thomaskiefer@users.noreply.github.com>
2025-04-06 00:21:50 +0200

    Create and run eval for reasoning and output separately and add DeepSeek-V3 log

---
+++ darkthoughtbench/_registry.py
@@ -0,0 +1,4 @@
+from darkthoughtbench.scorer import dark_pattern_score_output, dark_pattern_score_reasoning, overseer_output, overseer_reasoning
+from darkthoughtbench.task import darkthoughtbench
+
+__all__ = ["darkthoughtbench", "overseer_output", "overseer_reasoning", "dark_pattern_score_output", "dark_pattern_score_reasoning"]
---
+++ darkthoughtbench/task.py
@@ -0,0 +1,56 @@
+import pathlib
+
+import inspect_ai
+import inspect_ai.dataset
+import inspect_ai.model
+
+from darkthoughtbench import scorer
+from darkthoughtbench.solver import replay_deepseek_solver
+
+SYSTEM_PROMPT = "You are a helpful assistant."
+
+
+@inspect_ai.task
+def darkthoughtbench(
+    sample_ids: str | list[str] | None = None,
+    shuffle: bool = True,
+    assistant_temperature: float = 0,
+    overseer_models: str | list[str] = ["google/gemini-1.5-pro-latest", "anthropic/claude-3-5-sonnet-20241022", "openai/gpt-4o"],
+    overseer_temperature: float = 0,
+    dark_patterns: str | list[str] | None = None,
+):
+    overseer_models = (
+        overseer_models.split(",")
+        if isinstance(overseer_models, str)
+        else overseer_models
+    )
+    sample_ids = sample_ids.split(",") if isinstance(sample_ids, str) else sample_ids
+    dataset = inspect_ai.dataset.json_dataset(
+        json_file=str(pathlib.Path(__file__).parent / "darkbench_deepseek.jsonl"),
+        shuffle=shuffle,
+    )
+    if sample_ids:
+        dataset = dataset.filter(lambda x: x.id in sample_ids)
+
+    return inspect_ai.Task(
+        dataset=dataset,
+        solver=[replay_deepseek_solver()],
+        config=inspect_ai.model.GenerateConfig(
+            temperature=assistant_temperature,
+        ),
+        scorer=[
+            scorer.overseer_output(
+                model=overseer_model,
+                temperature=overseer_temperature,
+                dark_patterns=dark_patterns,
+            )
+            for overseer_model in overseer_models
+        ] + [
+            scorer.overseer_reasoning(
+                model=overseer_model,
+                temperature=overseer_temperature,
+                dark_patterns=dark_patterns,
+            )
+            for overseer_model in overseer_models
+        ],
+    )
